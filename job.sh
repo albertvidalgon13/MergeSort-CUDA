c8baf59d621a8a2a162a0b5e5801f64fde480aa0
Albert Vidal Gonzalez <82842276+albertvidalgon13@users.noreply.github.com>
2024-01-15 20:15:49 +0100

    Update job.sh

--- job.sh
+++ job.sh
@@ -11,14 +11,5 @@
 
 export PATH=/Soft/cuda/12.2.2/bin:$PATH
 
-# Para comprobar que funciona no es necesario usar matrices muy grandes
-# Con N = 1024 es suficiente
-
 ./mergesort.exe 10
-#./kernel4GPUs.exe 2048 N
-#./kernel4GPUs.exe 4096 N
-#./kernel4GPUs.exe 8192 N
 
-# nsys nvprof --print-gpu-trace ./mergesort.exe 20
-
-#nsys nvprof --print-gpu-trace ./kernel4GPUs.exe 1024 N
